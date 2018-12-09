a8498873dd4f8b465da56af0ecc8b62c96eacd40
Gabriel Martins <ga.smartins94@gmail.com>
2018-12-09 17:02:20 -0200

    Content Negociation XML/JSON

--- src/main/java/br/com/casadocodigo/loja/controller/ProdutoController.java
+++ src/main/java/br/com/casadocodigo/loja/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
@@ -46,6 +47,7 @@ public class ProdutoController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
+	@CacheEvict(value="produtosHome",allEntries=true)
 	public ModelAndView store(MultipartFile sumario,
 			                  @Valid Produto produto, 
 			                  BindingResult result,
@@ -80,4 +82,6 @@ public class ProdutoController {
 		return modelAndView;
 		
 	}
+	
+	
 }
